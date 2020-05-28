5b2bc043af4e8526cc3395d07eae7dd263015b77
Wilhelm Åkermark <willyt3hwhale@gmail.com>
2020-05-28 20:50:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,25 @@ from time import sleep, time
 from datetime import datetime, timedelta, timezone
 import asyncio
 
+text_message_timeout = 10
+voice_timeout = 5
+command_prefix = "!"
+
 
 class MyClient(discord.Client):
+    watchlist = {}
     watched_categories = []
     whitelist = []
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
         await self.cleanup()
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        if(message.author == message.guild.owner):
+            if(message.startswith(command_prefix + "watch ")):
+                watched_categories += int(message.split(' ')[1])
+
 
     async def on_voice_state_update(self, member, before, after):
 
@@ -23,24 +35,34 @@ class MyClient(discord.Client):
         is_empty = len(current_members) == 0
         channel_id = channel.id
         if(is_empty and category in self.watched_categories and channel_id not in self.whitelist):
-        	await channel.delete()
+            await channel.delete()
         
     async def on_guild_channel_create(self, channel):
+        if (channel.category_id not in self.watched_categories):
+            return
 
-    	if (channel.category_id not in self.watched_categories):
-    		return
+        if (channel.type == discord.ChannelType.voice):
+            await asyncio.sleep(voice_timeout)
+            if(len(channel.members) == 0):
+                await channel.delete()
 
-    	await asyncio.sleep(300)
-    	if(len(channel.members) == 0 or channel.type != discord.ChannelType.voice):
-    		await channel.delete()
+        elif (channel.type == discord.ChannelType.text):
+            await asyncio.sleep(text_message_timeout)
+            while(len(messages := await channel.history(limit=1, oldest_first=False).flatten()) > 0):
+                created_at = messages[0].created_at if messages[0].edited_at is None else messages[0].edited_at
+                timeout = ((created_at + timedelta(seconds=text_message_timeout)) - datetime.utcnow()).total_seconds()
+                if(timeout <= 0):
+                    break
+                await asyncio.sleep(timeout)
+            await channel.delete()
 
 
 
     async def cleanup(self):
-    	for server in self.guilds:
-    		for channel in server.channels:
-    			if(channel.category_id in self.watched_categories and channel.id not in self.whitelist and (len(channel.members) == 0 or channel.type != discord.ChannelType.voice)):
-    				await channel.delete()
+        for server in self.guilds:
+            for channel in server.channels:
+                if(channel.category_id in self.watched_categories and channel.id not in self.whitelist and (channel.type != discord.ChannelType.voice or len(channel.members) == 0 )):
+                    await channel.delete()
 
 
 
